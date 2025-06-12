5de4591de987039d0e66fef72e4b55e75115d928
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2025-06-12 20:08:28 +0200

    update: add TaskForm in App and update style for form

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Task} from './Task';
 import {TasksCollection} from "/imports/api/TasksCollection";
-import { useTracker, useSubscribe } from "meteor/react-meteor-data";
+import {useTracker, useSubscribe} from "meteor/react-meteor-data";
+import {TaskForm} from "/imports/ui/TaskForm";
 
 export type TaskType = {
     _id: string;
@@ -9,19 +10,19 @@ export type TaskType = {
 };
 
 export const App = () => {
-    const tasks:TaskType[] = useTracker(() => TasksCollection.find().fetch());
+    const tasks: TaskType[] = useTracker(() => TasksCollection.find().fetch());
     const isLoading = useSubscribe("tasks");
     if (isLoading()) {
         return <div>Loading...</div>;
     }
-
+<TaskForm/>
     return (
         <div>
             <h1>Welcome to Meteor!!!!!!!!!!!!!</h1>
 
             <ul>
-                {tasks.map((task:TaskType) => (
-                    <Task key={task._id} task={task} />
+                {tasks.map((task: TaskType) => (
+                    <Task key={task._id} task={task}/>
                 ))}
             </ul>
         </div>
